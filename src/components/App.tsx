be20e369be20cac6b92ba6dc342c0f00cafddd32
Miguel <migueltony9@gmail.com>
2025-02-27 20:28:14 -0600

    avanzando con nav

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,14 @@ import '../styles/App.css'
 const App = () => {
     return (
         <>
-            
+           <nav>
+                <div>Miguel</div>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Sobre mi</li>
+                    <li>Proyectos</li>
+                </ul>
+           </nav>
         </>
     )
 }
