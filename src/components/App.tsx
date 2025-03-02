d92d730a90d69103713d9b809ef80a2a5690b5ad
Miguel <migueltony9@gmail.com>
2025-03-02 01:39:45 -0600

    Layout about implementado

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,35 @@
-import '../styles/App.css'
+import '../styles/App.css';
+
 
 const App = () => {
     return (
         <>
-           <nav>
+            <span className='glow'></span>
+            <nav>
                 <div>Miguel</div>
                 <ul>
                     <li>Inicio</li>
                     <li>Sobre mi</li>
                     <li>Proyectos</li>
                 </ul>
-           </nav>
+            </nav>
+            <main className='sectionContainer'>
+                <section className='home'>
+                    <h1>Miguel A. Barrera</h1>
+                    <h2>Ingeniero en desarrollo de software</h2>
+                    <p>Aprendizaje constante para ser el mejor</p>
+                </section>
+                <section className="about">
+                    <div className='avatar'>
+                        hola
+                    </div>
+                    <div className='aboutInfoContainer'>
+                        <div>cv</div>
+                        <div>redes</div>
+                        <div>about</div>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
